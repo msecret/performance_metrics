735cec25fcfd73ff1a8059371be9e57b7d801c91
msecret <msegreto@miceover.com>
2015-11-23 17:38:11 -0800

    Total resources timing

---
+++ examples/resources.js
@@ -0,0 +1,27 @@
+
+(function(win) {
+
+  function resourcesTime() {
+    setTimeout(function() {
+      var resources = win.performance.getEntriesByType('resource'),
+          duration,
+          durations = [],
+          i = 0,
+          ilen;
+
+      console.log('resources', resources);
+      for (ilen = resources.length; i < ilen; i++) {
+        durations.push(resources[i].duration);   
+      }
+
+      duration = Math.max.apply(null, durations);
+
+      // TODO figure out what to put this in window.performance entries.
+      console.log('resources', duration);
+    }, 0);
+  };
+
+  // TODO need something to wait for async resources here, probably a plugin.
+  win.addEventListener('load', resourcesTime);
+
+})(window);
